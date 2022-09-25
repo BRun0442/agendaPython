b600ecc09a11c8c1a51e48a3dc7217a5d0c95ce1
BRun0442 <brunoosmar442@gmail.com>
2022-09-25 20:46:53 -0300

    preenchendo dicionario agenda

---
+++ agenda.py
@@ -0,0 +1,7 @@
+schedule = {
+    "lição Emerson": {"dia":26, "mes":9, "detalhes":"fazer agenda em python", "aula":"DS I"},
+    "lição APS": {"dia":3, "mes":10, "detalhes":"fazer diagrama de classes e comunicação e tempo", "aula":"APS"},
+    "trabalho de artes": {"dia":22, "mes":9, "detalhes":"elaborar uma coreografia", "aula":"artes"},
+    "trabalho nodeJs":{"dia":23, "mes":9, "detalhes":"fazer 9 exercícios mexendo com um Json", "aula":"PW II"},
+    "prova do alino":{"dia":19, "mes":9, "detalhes":"tipos de manutenção, MTBF, MTTR", "aula":"Organização industrial 4.0"}
+}
